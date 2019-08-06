7c61701053e060427a8c1c54ebac89dfd755e61b
Wasim Ansari <50865054+wasimansari661@users.noreply.github.com>
2019-08-06 14:58:37 +0530

    Update run-sequence.sh

--- configure-node-node/run-sequence.sh
+++ configure-node-node/run-sequence.sh
@@ -2,10 +2,6 @@ echo "Started master-pre-boot.sh"
 sudo sh master-pre-boot.sh
 echo "Completed master-pre-boot.sh"
 
-echo "Started master-post-boot-1"
-sudo sh master-post-boot-1.sh
-echo "Completed master-post-boot-1"
-
-echo "Started master-post-boot-2"
-sh master-post-boot-2.sh
-echo "Completed master-post-boot-2"
+echo "Started master-post-boot.sh"
+sudo sh master-post-boot.sh
+echo "Completed master-post-boot.sh"
