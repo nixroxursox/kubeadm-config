ae6f2bc64a56b0705f9c0f122563109923593a1f
Wasim Ansari <50865054+wasimansari661@users.noreply.github.com>
2019-08-13 21:47:12 +0530

    Update master-pre.sh

--- master-pre.sh
+++ master-pre.sh
@@ -1,37 +1,132 @@
-#this file will be used in Jenkins to run the installation on Kube Master
-#Run this as sudo
+#!/bin/bash
 
+u_name=`who | tr -s " "|cut -d" " -f1`
+
+if [[ `sudo -l -U $u_name` && `id -u` -eq 0 ]];
+then
+
+echo -e "\nUser <$u_name>  have sudo priviledge";
+echo -e "User <$u_name> is executing with sudo "
+
+echo -e "\n-------------------------------------\n"
+echo -e "Updateting the server\n"
 #configure the linux
-#sudo su
+
 apt-get update
 swapoff -a
 
-#install docker on master
+if [[ `echo $? -e 0` ]]
+then
+echo -e "\n-------------------------------------\n"
+echo -e "Installing Docker \n"
+
 apt-get install -y docker.io
 
-#install curl
+if [[ `echo $? -e 0` ]]
+then
+echo -e "\n-------------------------------------\n"
+echo -e "Installing curl and https \n"
+
 apt-get update && apt-get install -y apt-transport-https curl
 
-#Add the key for Kubernetes
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
+if [[ `echo $? -e 0` ]]
+then
+echo -e "\n-------------------------------------\n"
 
-#Add the kubernetes resourcces in the repo list
+echo -e "\nAdding kubernetes resourcces in the repo list"
+
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-#update the repo
+
+if [[ `echo $? -e 0` ]]
+then
+echo -e "\n-------------------------------------\n"
+echo -e "Updateting the server\n"
 apt-get update
 
-#install kubernetes tools
+if [[ `echo $? -e 0` ]]
+then
+echo -e "\n-------------------------------------\n"
+echo -e "Installing the kubelet,kubeadm,kubectl\n"
+
 apt-get install -y kubelet kubeadm kubectl
 
-#environment configuration of Kubernetes
+
+if [[ `echo $? -e 0` ]]
+then
+echo -e "\n-------------------------------------\n"
+echo -e "Environment configuration of Kubernetes\n"
+
 if [ ! $(cat /etc/systemd/system/kubelet.service.d/10-kubeadm.conf | grep "Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs"" ) ];
 then
 echo "Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs""  >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 else
 echo "Environment driver configured"
 fi;
-#reboot vm
+
+if [[ `echo $? -e 0` ]]
+then
+echo -e "\n-------------------------------------\n"
+
+echo -e "All steps completed sucessfully now rebooting the server\n"
+
 shutdown --reboot +1 "System is going down for reboot in 1 minute"
+
+
+else
+echo -e "Environment configuration of Kubernetes\n"
+exit 9
+fi;
+
+
+else
+echo -e "Error in the kubelet,kubeadm,kubectl\n"
+exit 9
+fi;
+
+
+
+
+else
+echo -e "Error in the kubelet,kubeadm,kubectl\n"
+exit 9
+fi;
+
+
+
+
+else
+echo -e "Error in Adding kubernetes resourcces in the repo list\n"
+exit 9
+fi;
+
+
+
+
+
+else
+echo -e "Error in in installing curl and https\n"
+exit 9
+fi;
+
+
+else
+echo -e "Error in in installing Docker\n"
+exit 9
+fi;
+
+
+else
+
+echo -e "Error in in Updateting the server\n"
+exit 9
+fi;
+
+else
+echo -e "\nUser <$u_name> dont have sudo priviledge"
+echo -e "Or"
+echo -e "Not using sudo keyword"
+exit 9
+fi;
